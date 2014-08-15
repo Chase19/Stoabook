c5346200c50a6680757e8e2d6a925cad65aed3dd
Chase19 <chaseofchase@yahoo.com>
2014-08-14 20:48:32 -0700

    Added Custom Routes for Authentication and Feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Stoabook::Application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get "sign_up", to: "devise/registrations#new", as: :sign_up
+    get "sign_in", to: "devise/sessions#new", as: :sign_in
+    #get "#{current_user}", to: "devise/registrations#edit"
+  end
+  
   resources :statuses
+  get "update_status", to: "statuses#new", as: :update_status
   root to: 'statuses#index'
 end
